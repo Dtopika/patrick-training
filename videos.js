550dfef7acb622193a64c19959a7f87b33833f5b
Johan Bernal <cristianrodriguez914@gmail.com>
2026-09-15 21:57:22 -0500

    feat: add curated training video library

---
+++ videos.js
@@ -0,0 +1,12 @@
+window.PATRICK_VIDEOS={
+  "Patrick":{youtubeId:"wqoYc4VZCOg",title:"Teach your puppy his name",source:"Kikopup"},
+  "Komm":{youtubeId:"rjd99MlmqqI",title:"Puppy recall: come when called",source:"Kikopup"},
+  "Hier":{youtubeId:"rjd99MlmqqI",title:"Puppy recall: come when called",source:"Kikopup"},
+  "Sitz":{youtubeId:"pVJDbLgublM",title:"Following a lure / teaching Sit and Down",source:"4PawsUniversity"},
+  "Platz":{youtubeId:"pVJDbLgublM",title:"Following a lure / teaching Sit and Down",source:"4PawsUniversity"},
+  "Aus":{youtubeId:"gZvkyAFi7tc",title:"How to train DROP IT",source:"Kikopup"},
+  "Lass es":{youtubeId:"Re66qOiso28",title:"Train your dog or puppy Leave It",source:"Kikopup"},
+  "Bleib":{youtubeId:"Vk4PPcE1CqY",title:"Teach your dog to stay",source:"Kikopup"},
+  "Nein":{youtubeId:"TBvPaqMZyo8",title:"Positive interrupter",source:"Kikopup"},
+  "Box":{youtubeId:"dUzF0g0PwY4",title:"Teach your puppy to enjoy the crate",source:"Kikopup"}
+};
