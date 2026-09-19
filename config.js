e5dd682c37a9b519f26221d3a8df34c54ed646c9
Johan Bernal <cristianrodriguez914@gmail.com>
2026-09-19 09:15:22 -0500

    chore: bump Patrick to v6.1.0

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 (()=>{'use strict';
   const config=Object.freeze({
     APP_NAME:'Patrick Training',
-    APP_VERSION:'6.0.0',
-    BACKUP_SCHEMA_VERSION:6,
-    SESSION_SCHEMA_VERSION:6,
+    APP_VERSION:'6.1.0',
+    BACKUP_SCHEMA_VERSION:7,
+    SESSION_SCHEMA_VERSION:7,
     DB_VERSION:1,
     CACHE_PREFIX:'patrick-training-',
-    CACHE_NAME:'patrick-training-v6.0.0-r2',
+    CACHE_NAME:'patrick-training-v6.1.0-r1',
     BACKUP_MAX_BYTES:2*1024*1024
   });
   globalThis.PATRICK_CONFIG=config;
