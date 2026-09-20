b3799b81a447ec8f0b2f0841b5c86a58669673d6
Johan Bernal <cristianrodriguez914@gmail.com>
2026-09-20 12:38:43 -0500

    chore(cache): align v7.5 cache name with r3

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
     SESSION_SCHEMA_VERSION:9,
     DB_VERSION:1,
     CACHE_PREFIX:'patrick-training-',
-    CACHE_NAME:'patrick-training-v7.5.0-r2',
+    CACHE_NAME:'patrick-training-v7.5.0-r3',
     BACKUP_MAX_BYTES:2*1024*1024
   });
   globalThis.PATRICK_CONFIG=config;
