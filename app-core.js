0fef9cde0634f643935889547913330bdc2d5793
Johan Bernal <cristianrodriguez914@gmail.com>
2026-09-21 14:59:14 -0500

    chore(release): bump v7.7 asset revision

--- app-core.js
+++ app-core.js
@@ -4,7 +4,7 @@ const I18N=window.PatrickI18n;
 let CONFIG=window.PATRICK_CONFIG||null;
 let ENGINE=window.PatrickTrainingEngine||null;
 let BACKUP_SCHEMA=window.PatrickBackupSchema||null;
-const V6_ASSET_TAG='v760-r1';
+const V6_ASSET_TAG='v770-r1';
 
 function loadPatrickDependency(src,isReady){
   if(isReady())return Promise.resolve();
