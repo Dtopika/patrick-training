463082de5d823e2e04b6a7b24c47f0da4165bb7a
Johan Bernal <cristianrodriguez914@gmail.com>
2026-09-21 15:55:42 -0500

    chore(release): prepare Patrick Training v7.9.0

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 (()=>{'use strict';
   const config=Object.freeze({
     APP_NAME:'Patrick Training',
-    APP_VERSION:'7.8.1',
+    APP_VERSION:'7.9.0',
     BACKUP_SCHEMA_VERSION:13,
     SESSION_SCHEMA_VERSION:9,
     DB_VERSION:1,
     MAX_DOG_AGE_MONTHS:240,
     CACHE_PREFIX:'patrick-training-',
-    CACHE_NAME:'patrick-training-v7.8.1-r1',
+    CACHE_NAME:'patrick-training-v7.9.0-r1',
     BACKUP_MAX_BYTES:2*1024*1024
   });
   globalThis.PATRICK_CONFIG=config;
